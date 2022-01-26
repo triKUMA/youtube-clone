294815eb60defaabf8ad1d1d7b81889177081cc6
tri/KUMA <trikuma@hotmail.com>
2022-01-26 15:42:59 +0800

    Made feed banners closeable.

--- src/FeedBanner.tsx
+++ src/FeedBanner.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./FeedBanner.css";
 import YouTubeIcon from "@mui/icons-material/YouTube";
 import ClearIcon from "@mui/icons-material/Clear";
@@ -12,25 +12,65 @@ interface FeedBannerProps {
 }
 
 function FeedBanner(props: FeedBannerProps) {
+  const [display, setDisplay] = useState(true);
+
   return (
-    <div className={"feedBanner" + (props.darkVariant ? " darkVariant" : "")}>
-      <img src={props.imgSrc} />
-      <div className="details">
-        <div className="logo">
-          <YouTubeIcon className="logo-icon" />
-          <p className="logo-title">YouTube</p>
-          <p className="logo-extra">Featured</p>
-        </div>
-        <p className="banner-title">{props.title}</p>
-        <p className="banner-description">{props.desc}</p>
-      </div>
-      <div className="banner-right">
-        <button className="cta">{props.cta}</button>
-        <button className="minimize">
-          <ClearIcon className="minimize-icon" />
-        </button>
-      </div>
+    <div
+      className={
+        display
+          ? "feedBanner" + (props.darkVariant ? " darkVariant" : "")
+          : "closedBanner"
+      }
+    >
+      {display ? (
+        <>
+          <img src={props.imgSrc} />
+          <div className="details">
+            <div className="logo">
+              <YouTubeIcon className="logo-icon" />
+              <p className="logo-title">YouTube</p>
+              <p className="logo-extra">Featured</p>
+            </div>
+            <p className="banner-title">{props.title}</p>
+            <p className="banner-description">{props.desc}</p>
+          </div>
+          <div className="banner-right">
+            <button className="cta">{props.cta}</button>
+            <button
+              className="minimize"
+              onClick={() => {
+                setDisplay(false);
+              }}
+            >
+              <ClearIcon className="minimize-icon" />
+            </button>
+          </div>
+        </>
+      ) : (
+        <>
+          <p>Got it, message hidden</p>
+        </>
+      )}
     </div>
+
+    // <div className={"feedBanner" + (props.darkVariant ? " darkVariant" : "")}>
+    //   <img src={props.imgSrc} />
+    //   <div className="details">
+    //     <div className="logo">
+    //       <YouTubeIcon className="logo-icon" />
+    //       <p className="logo-title">YouTube</p>
+    //       <p className="logo-extra">Featured</p>
+    //     </div>
+    //     <p className="banner-title">{props.title}</p>
+    //     <p className="banner-description">{props.desc}</p>
+    //   </div>
+    //   <div className="banner-right">
+    //     <button className="cta">{props.cta}</button>
+    //     <button className="minimize">
+    //       <ClearIcon className="minimize-icon" />
+    //     </button>
+    //   </div>
+    // </div>
   );
 }
 
