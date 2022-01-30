ff67b4dcc4efe95ea768886b8ed3b96f11a39b33
tri/KUMA <trikuma@hotmail.com>
2022-01-30 19:30:52 +0800

    Added working scroll buttons on topics bar.

--- src/TopicsBar.tsx
+++ src/TopicsBar.tsx
@@ -1,11 +1,31 @@
 import "./TopicsBar.css";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 
 function TopicsBar() {
+  const topicsBar = document.getElementById("topicsBar");
+
+  if (topicsBar !== null) {
+    topicsBar.addEventListener("scroll", () => {
+      if (topicsBar.scrollLeft === 0) {
+        document.getElementById("scroll-left")?.toggleAttribute("disable");
+      } else if (topicsBar.scrollLeft === topicsBar.scrollWidth) {
+        document.getElementById("scroll-right")?.toggleAttribute("disable");
+      }
+    });
+  }
+
   return (
-    <div className="topicsBar">
-      {/* {[...Array(20)].map((value: undefined, index: number) => (
-        <button className="topic">Topic {index + 1}</button>
-      ))} */}
+    <div className="topicsBar" id="topicsBar">
+      <button
+        className="scroll-left"
+        id="scroll-left"
+        onClick={() => {
+          topicsBar?.scrollBy({ top: 0, left: -150, behavior: "smooth" });
+        }}
+      >
+        <ChevronLeftIcon />
+      </button>
       <button className="topic active">All</button>
       <button className="topic">Live</button>
       <button className="topic">Lo-fi</button>
@@ -20,6 +40,15 @@ function TopicsBar() {
       <button className="topic">Sketch comedy</button>
       <button className="topic">Scene</button>
       <button className="topic">Comedies</button>
+      <button
+        className="scroll-right"
+        id="scroll-right"
+        onClick={() => {
+          topicsBar?.scrollBy({ top: 0, left: 150, behavior: "smooth" });
+        }}
+      >
+        <ChevronRightIcon />
+      </button>
     </div>
   );
 }
